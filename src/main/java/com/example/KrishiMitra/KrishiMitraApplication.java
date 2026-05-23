5885528770789f553d8bd7c6f2ce150683137812
Vaishnavi <thavaivaishnavi555@gmail.com>
2026-05-23 21:57:53 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/example/KrishiMitra/KrishiMitraApplication.java
@@ -0,0 +1,13 @@
+package com.example.KrishiMitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishiMitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrishiMitraApplication.class, args);
+	}
+
+}
